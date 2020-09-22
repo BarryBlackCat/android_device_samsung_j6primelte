6231fa322691cd46567a462a11b9c68bb6d76d39
BarryBlackCat <everydayonechoice@gmail.com>
2020-09-22 10:01:55 -0300

    Welcome Fluid to J6+

---
+++ fluid_config.mk
@@ -0,0 +1,18 @@
+# Gapps flags
+TARGET_INCLUDE_GAPPS := true
+TARGET_GAPPS_ARCH := arm
+IS_PHONE := true
+
+# Whether or not to disable the default camera
+DISABLE_DEFAULT_CAMERA := false
+
+# Whether or not target supports google recoder
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+
+# Build Type
+CUSTOM_BUILD_TYPE := OFFICIAL
+
+#BOOT ANIMATION
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
